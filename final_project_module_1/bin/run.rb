bcae36a1baaa5be338d47dfb9303efe0442e5477
Janaehall <51344260+Janaehall@users.noreply.github.com>
2019-10-25 01:08:08 -0500

    Add files via upload

---
+++ final_project_module_1/bin/run.rb
@@ -0,0 +1,34 @@
+require_relative '../config/environment.rb'
+CLEAR
+welcome
+run = true
+
+while run
+    input = get_input(PROMPT.ask(players_or_crimes, echo:true))
+    case input
+    when 1
+        display_name_table  
+        player = Player.find_or_create_by(name: get_input(gets.chomp))
+        view_player_arrests(player)
+        menu_for_player(player)
+    when 2
+        display_crime_table
+        puts "choose crime:"
+        crime = take_crime_input(gets.chomp)
+        puts crime.category
+        crime_choices(crime)
+    else
+        puts "Wrong input. Must be 1 or 2. Now you have to start over..\n\n".yellow
+    end
+    puts "\n\n\nEnter 'Yes' or 'Y' to go again...".red
+    puts "Enter anyting else to exit".red
+    ans = get_input(gets.chomp)
+if ans == "Yes" || ans == "Y"
+    run = true
+else 
+    sign_off_message
+    run = false
+end
+end
+CLEAR
+
